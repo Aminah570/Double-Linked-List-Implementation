0d0766fb11383fe2ed256753cf37cafa5e817848
Aminah570 <aminahrehman123@icloud.com>
2026-06-28 21:19:55 -0400

    Main function for doubly linked list

---
+++ driver.cpp
@@ -0,0 +1,14 @@
+#include "DoublyLL.h"
+using namespace std;
+
+int main(){
+    DLL aList;
+    aList.insertAtHead("FIRST");
+    aList.insertAtHead("SECOND");
+    aList.insertAtHead("THIRD");
+    aList.addInBetween("THIRD", "FOURTH");
+    aList.addInBetween("SECOND", "EXCEPTION");
+    aList.dump();
+    cout << endl;
+    return 0;
+}
